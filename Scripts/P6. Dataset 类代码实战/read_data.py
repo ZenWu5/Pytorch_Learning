7dfe4792be1b0b3d097c0d8ed29d661fc0c74e9b
ZenWu5 <19871775905@163.com>
2025-09-26 02:36:32 +0800

    P6 completed

--- Scripts/P6. Dataset 类代码实战/read_data.py
+++ Scripts/P6. Dataset 类代码实战/read_data.py
@@ -21,9 +21,19 @@ class ImageDataset(Dataset):
 # 使用示例
 if __name__ == "__main__":
     image_root_path = "Dataset/hymenoptera_data/train" # 相对路径
-    label = "ants"
+    ants_label = "ants"
+    bees_label = "bees"
     
-    dataset = ImageDataset(image_root_path, label)
-    # for img, lbl in dataset: # 遍历数据集
-        # print(f"Image shape: {img.shape}, Label: {lbl}")
-    print(f"Data type: {type(dataset)}, Length: {dataset.__len__()}") # 输出数据集类型和长度，这里可以看到使用opencv读取的图像是numpy数组
+    # 创建数据集实例
+    ants_dataset = ImageDataset(image_root_path, ants_label)
+    bees_dataset = ImageDataset(image_root_path, bees_label)
+    
+    # 合并两个数据集
+    train_dataset = ants_dataset + bees_dataset
+    
+    # 遍历数据集，打印图像形状和标签
+    # for img, lbl in train_dataset:
+    #     print(f"Image shape: {img.shape}, Label: {lbl}")
+    
+    # 输出数据集类型和长度，这里可以看到Dataset类型是: list[tuple(data: np.ndarray, label: str)]
+    print(f"Data Example: {train_dataset[0]}, Length: {train_dataset.__len__()}") 
